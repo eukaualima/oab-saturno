3263f3562cdcb39385bd48e149e2ebbe5c207867
Kauã Lima <mavericksamp2018@gmail.com>
2023-12-21 20:45:24 -0300

    :zap: atualização dos eventos

---
+++ eventos/botoes.js
@@ -0,0 +1,39 @@
+/**
+ * @file Evento ativado quando um usuário requere um botão.
+ * @author Kauã Lima
+ * @since OABSRP-0.1
+ * @version OABSRP-0.1
+ */
+
+module.exports =
+{
+    name: "interactionCreate",
+
+    async execute (interaction)
+    {
+        const { client } = interaction;
+
+        if (!interaction.isButton()) return;
+
+        const botao = client.selectCommands.get(interaction.customId);
+
+        if (!botao)
+        {
+            await require("../padroes/padrao_erro_botao");
+        }
+
+        try
+        {
+            await botao.execute(interaction);
+        }
+        catch (err)
+        {
+            console.error(err);
+
+            await interaction.reply({
+                content: `${interaction.user.displayName}, este comando está com erro. Comuniquei o meu desenvolvedor.`,
+                ephemeral: true
+            })
+        }
+    },
+}
---
+++ eventos/menus.js
@@ -0,0 +1,39 @@
+/**
+ * @file Evento ativado quando um usuário requere um menu.
+ * @author Kauã Lima
+ * @since OABSRP-0.1
+ * @version OABSRP-0.1
+ */
+
+module.exports =
+{
+    name: "interactionCreate",
+
+    async execute (interaction)
+    {
+        const { client } = interaction;
+
+        if (!interaction.isAnySelectMenu()) return;
+
+        const menu = client.selectCommands.get(interaction.customId);
+
+        if (!menu)
+        {
+            await require("../padroes/padrao_erro_menu");
+        }
+
+        try
+        {
+            await menu.execute(interaction);
+        }
+        catch (err)
+        {
+            console.error(err);
+
+            await interaction.reply({
+                content: `${interaction.user.displayName}, este comando está com erro. Comuniquei o meu desenvolvedor.`,
+                ephemeral: true
+            })
+        }
+    },
+}
